0d9ed9f4d88cabe24e8c52d646860cb5cf51d90b
Tarun Chauhan <tarun.chauhan@student.kpu.ca>
2026-03-11 22:00:42 -0700

    Learned from the book about the binary conversion this book looks good. Book name is "Introduction programming in Java second edition from Robert Sedgewick"

---
+++ src/week_01/book/chapter01/Binary.java
@@ -0,0 +1,30 @@
+package week_01.book.chapter01;
+
+import java.util.Scanner;
+
+//11 March,2026
+// the program to convert to the binary from the decimal
+public class Binary {
+    static void main() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        int power = 1;
+        while (power <= num/2) {
+            power *= 2;
+        }
+
+        while (power > 0) {
+            if( power > num ){
+                System.out.print(0);
+            }
+            else{
+                System.out.print(1);
+                num = num - power;
+
+            }
+            power = power/2;
+
+        }
+    }
+}
